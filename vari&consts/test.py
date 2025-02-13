b8408f5c275f58826cc7503fd4fa2cb39bc421d8
jesse <nitchsx@gmail.com>
2025-02-13 00:35:29 -0300

    feat: vari and consts

---
+++ vari&consts/test.py
@@ -0,0 +1,5 @@
+name , age = "jesse" , 20
+print(f'Meu nome é {name} e tenho {age} anos')
+
+# não existe palavra reservado para declarar uma constante em python , a forma de deixar isso claro , se usa o nome da vari de forma maiscula apenas kkkkkk(porcaria de linguagem)
+AGE = 21
