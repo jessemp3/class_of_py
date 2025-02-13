e27a523e4bcd83b809075bc171c5b0178d4434b4
jesse <nitchsx@gmail.com>
2025-02-13 00:53:55 -0300

    feat: a

---
+++ vari&consts/conversao.py
@@ -0,0 +1,10 @@
+# float 
+preco = 10
+precoa = float(preco)
+print(precoa)
+
+# string
+
+string = (str(preco))
+print(string)
+
---
+++ vari&consts/input.py
@@ -0,0 +1,4 @@
+nome = input("qual seu nome:")
+sobrenome = "alves"
+print(f'seu nome é {nome}' , end="...\n")
+print(nome ,sobrenome , sep="#" )
