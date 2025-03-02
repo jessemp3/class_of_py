19b402acbe742bec81e5fe2e449078f90431fcec
jesse <nitchsx@gmail.com>
2025-03-02 02:08:28 -0300

    feat: class from operadores

---
+++ operadores/index.py
@@ -0,0 +1,24 @@
+preco1 = 10
+preco2 = 20
+
+print(preco1 + preco2)
+print(preco1 - preco2)
+print(preco1 * preco2)
+print(preco1 / preco2)
+print(preco1 % preco2)
+print(preco1 ** preco2)
+print(preco1 // preco2)
+
+x = 10 + 5 * 4 
+print(x)
+
+
+name = "jesse"
+name1 = name
+
+print(name is name1) # operador de identidade
+print(name is not name1) # operador de identidade
+
+curso = "curso de python"
+
+print("curso" in curso) # operador de associação
